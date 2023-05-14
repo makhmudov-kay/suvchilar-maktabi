fe7b16eff36c1b63ca112df971b79b0c27122628
MaxKay <makhmudov.kay@gmail.com>
2023-05-15 01:06:04 +0500

    add editing modal

--- src/app/modules/admin/modules/dashboard/modules/applications/applications.component.ts
+++ src/app/modules/admin/modules/dashboard/modules/applications/applications.component.ts
@@ -1,4 +1,8 @@
 import {
+  District,
+  Region,
+} from './../../../../../main/components/application-form/models/region-and-districts.response';
+import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
@@ -6,13 +10,8 @@ import {
 } from '@angular/core';
 import { ApplicationsService } from './services/applications.service';
 import { Application } from './models/application.response';
-
-interface Person {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-}
+import { RegionsAndDistrictsService } from 'src/app/modules/main/components/application-form/services/regions-and-districts.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-applications',
@@ -21,17 +20,133 @@ interface Person {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ApplicationsComponent implements OnInit {
+  /**
+   */
+  isVisible = false;
+
+  /**
+   */
   data!: Application[];
 
+  /**
+   */
+  districts: District[] = [];
+
+  /**
+   */
+  form!: FormGroup;
+
+  /**
+   *
+   */
+  region!: Region[];
+
+  /**
+   *
+   */
+  districtFromRegion!: District[];
+
+  /**
+   *
+   */
+  selectedRegion!: number;
+
+  /**
+   *
+   * @param $applications
+   * @param $regionAndDistrict
+   * @param cd
+   */
   constructor(
     private $applications: ApplicationsService,
+    private $regionAndDistrict: RegionsAndDistrictsService,
+    private fb: FormBuilder,
     private cd: ChangeDetectorRef
   ) {}
 
-  ngOnInit(): void {
+  /**
+   *
+   */
+  private getApplications() {
     this.$applications.getApplicationsList().subscribe((result) => {
       this.data = result.data;
       this.cd.markForCheck();
     });
+  }
+
+  /**
+   *
+   */
+  private getDistricts() {
+    this.$regionAndDistrict.getRegionAndDistricts().subscribe((result) => {
+      const regions = result.data;
+      regions.forEach((region) => {
+        region.districts.forEach((el) => {
+          this.districts.push(el);
+        });
+      });
+      this.cd.markForCheck();
+    });
+  }
+
+  /**
+   *
+   */
+  private editingData(editingData?: Application) {
+    this.form = this.fb.group({
+      phone: [editingData?.phone],
+      l_name: [editingData?.l_name],
+      f_name: [editingData?.f_name],
+      farm_name: [editingData?.farm_name],
+      district_id: [editingData?.district_id],
+      farm_type: [editingData?.farm_type],
+    });
+  }
+
+  /**
+   *
+   */
+  ngOnInit(): void {
+    this.getDistricts();
+    this.getApplications();
+    this.editingData();
+  }
+
+  /**
+   *
+   * @param districtId
+   * @returns
+   */
+  filteredDistrict(districtId: number) {
+    const district = this.districts.find((el) => el.id === districtId);
+
+    this.cd.markForCheck();
+    if (district) {
+      return district.name;
+    }
+    return;
+  }
+
+  edit(data: Application) {
+    this.isVisible = true;
+    this.editingData(data);
+    this.cd.markForCheck();
+  }
+
+  handleOk() {
+    this.isVisible = false;
+  }
+
+  handleCancel() {
+    this.isVisible = false;
+  }
+
+  /**
+   *
+   * @param region
+   */
+  regionChange(region: Region) {
+    this.districtFromRegion = region.districts;
+    this.cd.markForCheck();
   }
 }
--- src/app/modules/admin/modules/dashboard/modules/applications/applications.module.ts
+++ src/app/modules/admin/modules/dashboard/modules/applications/applications.module.ts
@@ -8,12 +8,18 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import {TranslateModule} from '@ngx-translate/core';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @NgModule({
   imports: [
     CommonModule,
     ApplicationsRoutes,
     TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     /* Ng-ZORRO */
     NzTableModule,
@@ -21,6 +27,9 @@ import {TranslateModule} from '@ngx-translate/core';
     NzInputModule,
     NzButtonModule,
     NzIconModule,
+    NzModalModule,
+    NzFormModule,
+    NzSelectModule,
   ],
   declarations: [ApplicationsComponent],
 })
